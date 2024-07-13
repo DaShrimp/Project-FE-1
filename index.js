65a29be4710322cc908a71f0e8eaee4b94113482
DaShrimp <nhatminhvo2311@gmail.com>
2024-07-13 12:21:45 +0700

    update all

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const dropmenu = document.querySelectorAll('.dropmenu');
-dropmenu.forEach(dropmenu => {
+const dropmenus = document.querySelectorAll('.dropmenu');
+
+dropmenus.forEach(dropmenu => {
     const select = dropmenu.querySelector('.select');
     const caret = dropmenu.querySelector('.caret');
     const menu = dropmenu.querySelector('.menu');
-    const options = dropmenu.querySelector('.menu li');
+    const options = menu.querySelectorAll('li');
     const selected = dropmenu.querySelector('.selected');
-    
-    select.addEventListener('click', () =>{
+
+    select.addEventListener('click', () => {
         select.classList.toggle('select-clicked');
         caret.classList.toggle('caret-rotate');
         menu.classList.toggle('menu-open');
     });
 
     options.forEach(option => {
-        option.addEventListener('click', ()=> {
-            selected.innerText = option.innerText;
+        option.addEventListener('click', () => {
+            selected.innerText = option.querySelector('a').innerText;
             select.classList.remove('select-clicked');
             caret.classList.remove('caret-rotate');
             menu.classList.remove('menu-open');
-            options.forEach(option => {
-                option.classList.remove('active');
+            options.forEach(opt => {
+                opt.classList.remove('active');
             });
             option.classList.add('active');
         });
